2646229f36b8fdda89f086e804a2fae660a631fb
Laurent Bassin <laurent@bassin.info>
2015-12-10 08:57:16 +0100

    Ajout fichiers base

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+        <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
+        <link type="text/css" rel="stylesheet" href="{{asset('style/desktop.css')}}">
+        <script src="{{asset('js/jquery/jquery.1.11.3.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+
+    </head>
+    <body>
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-5" style="background-color:blue">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+          </div>
+        </div>
+    </body>
+</html>
