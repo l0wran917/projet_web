8c9af1644f9755e509236da586377327d550717b
Laurent Bassin <laurent@bassin.info>
2015-12-14 23:33:45 +0100

    Vue login dans welcome + js

---
+++ public/js/login.js
@@ -0,0 +1,58 @@
+$(window).load(function(){
+
+  $(".login #nextLogin").on("click", nextSubmit);
+
+  $(".login .iconPrevious").on("click", previousSubmit);
+
+})
+
+
+function nextSubmit(){
+
+  var mail = $("#inputEmail").val();
+  var prenomDotNom = mail.split("@")[0];
+  var prenom = prenomDotNom.split(".")[0];
+  var nom = prenomDotNom.split(".")[1];
+
+  prenom = prenom[0].toUpperCase() + prenom.slice(1);
+  nom = nom[0].toUpperCase() + nom.slice(1);
+
+  $(".login .first-panel").css({
+    'transition': '100ms linear',
+    'transform': 'translate(-150%, 0)'
+  });
+
+  $(".login .second-panel").css({
+    'transition': '100ms linear',
+    'transform': 'translate(0%, -100%)'
+  });
+
+  $(".login .iconPrevious").css({
+    'display' : 'block'
+  });
+
+  $('.login .prenom').text(prenom);
+  $('.login .nom').text(nom);
+
+}
+
+function previousSubmit(){
+
+  $(".login .first-panel").css({
+    'transition': '100ms linear',
+    'transform': 'translate(0%, 0)'
+  });
+
+  $(".login .second-panel").css({
+    'transition': '100ms linear',
+    'transform': 'translate(100%, -100%)'
+  });
+
+  $(".login .iconPrevious").css({
+    'display' : 'none'
+  });
+
+  $('.login .prenom').text("");
+  $('.login .nom').text("");
+
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,21 +3,74 @@
     <head>
         <title>Laravel</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
         <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
         <link type="text/css" rel="stylesheet" href="{{asset('style/desktop.css')}}">
         <script src="{{asset('js/jquery/jquery.1.11.3.min.js')}}"></script>
         <script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+        <script src="{{asset('js/login.js')}}"></script>
 
     </head>
     <body>
-        <div class="container">
-          <div class="row">
-            <div class="col-xs-5" style="background-color:blue">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <nav class="navbar navbar-inverse navbar-default">
+            <div class="container-fluid">
+              <div class="navbar-header">
+                <!--  -->
+              </div>
+            </div>
+          </nav>
+
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-xs-10 col-xs-offset-1 text-center">
+                <h1>Gestion des stages</h1>
+                <h3>Université Paris Sud</h3>
+              </div>
+            </div>
+            <div class="row" >
+              <div class="col-xs-6 col-xs-offset-3 col-md-4 col-md-offset-4 login">
+
+                <div class="iconPrevious">
+                  <span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span>
+                </div>
+
+                <div class="personalPicture">
+                  <img src="img/testImg.png" alt="" />
+                </div>
+
+                <div class="row">
+                  <div class="col-xs-6 col-xs-offset-3 infoUser">
+                      <span class="prenom"></span></br>
+                      <span class="nom"></span>
+                  </div>
+                </div>
+
+                <div class="first-panel">
+                  <div class="row">
+                    <div class="col-xs-10 col-xs-offset-1">
+                      <input type="text" class="form-control input-lg" name="inputEmail" id="inputEmail" value="" placeholder="prenom.nom@u-psud.fr">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-8 col-xs-offset-2">
+                      <input type="button" class="form-control btn btn-primary btn-block" id="nextLogin" value="Suivant" >
+                    </div>
+                  </div>
+                </div>
+
+                <div class="second-panel">
+                  <div class="row">
+                    <div class="col-xs-10 col-xs-offset-1">
+                      <input type="password" class="form-control input-lg" name="password" value="" placeholder="***********">
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-xs-8 col-xs-offset-2">
+                      <input type="button" class="form-control btn btn-primary btn-block" id="submitLogin" value="Valider" >
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
     </body>
 </html>
