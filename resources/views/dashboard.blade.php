bce1ecbc75e74583420e861440a6071ca31951b5
Bryan <bryan.boukari@gmail.com>
2015-12-17 14:31:24 +0100

    Creation du dashboard

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Dashboard</title>
+
+        <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
+        <link type="text/css" rel="stylesheet" href="{{asset('style/dashboard.css')}}">
+        <script src="{{asset('js/jquery/jquery.1.11.3.min.js')}}"></script>
+        <script src="{{asset('js/bootstrap/bootstrap.min.js')}}"></script>
+
+    </head>
+    <body>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-xs-3 text-center dashboard-menu">
+            <div class="col-xs-12 text-center infoUser">
+dddd
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-xs-10 col-xs-offset-3 durouge">
+            <!-- -->
+          </div>
+        </div>
+      </div>
+    </body>
+</html>
