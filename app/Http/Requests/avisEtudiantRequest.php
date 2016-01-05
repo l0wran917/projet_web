01116d12a9027941bd2a64741f8c4738973015e1
Bryan <bryan.boukari@gmail.com>
2016-01-05 15:27:36 +0100

    Ajout du request avisEtudiant

---
+++ app/Http/Requests/avisEtudiantRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use Auth;
+
+class avisEtudiantRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+      if(Auth::check()){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'remenerationStage' => 'required',
+            //'montantRemuneration' => 'required',
+            'encadrageInfomaticien' => 'required',
+            'appelInformaticien' => 'required',
+            'travailSeul' => 'required',
+            'typeMateriel' => 'required',
+            'typeSysteme[]' => 'required',
+            'objetStage[]' => 'required'
+    }
+}
