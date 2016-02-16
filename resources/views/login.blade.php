798816184c22b995998ea3d1e71261d1b1251b07
Laurent Bassin <laurent@bassin.info>
2016-02-16 22:39:51 +0100

    Title page login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
+        <title>Gestion Stage IUT Orsay</title>
 
         <link type="text/css" rel="stylesheet" href="{{asset('style/bootstrap.min.css')}}">
         <link type="text/css" rel="stylesheet" href="{{asset('style/desktop.css')}}">
